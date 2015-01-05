daa45b052ff46e9c49742e977c5f748b657e1955
Brady Dowling <bradydowling@gmail.com>
2015-01-05 04:17:46 +0000

    Changed formatting on page

--- page.php
+++ page.php
@@ -3,18 +3,19 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 
-
-  <article>
-    <div class="container">
-	  <?php
+  <div class="container">
+    <?php
     $thumb = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'full' );
     $url = $thumb['0'];
     ?>
     <div class="jumbotron first page" style="background-image:url(<?=$url?>);"></div>
-        <div class="row col-md-offset-1 col-md-10">
-          <h1 class="text-center"><?php the_title(); ?></h1>
-            <?php the_content(); ?>
-        </div>
+  </div>
+  <article>
+    <div class="container">
+      <div class="row col-md-offset-1 col-md-10">
+        <h1 class="text-center"><?php the_title(); ?></h1>
+          <?php the_content(); ?>
+      </div>
     </div>
   </article>
 
